c830bc6c10bc98e107dfa4c338ab0d586b5a9d15
usomarriba <usomarriba@kx.com>
2024-01-27 17:55:48 -0500

    Add player platforming mechanics.

--- Combat/Player.gd
+++ Combat/Player.gd
@@ -1,6 +1,9 @@
-extends Node
-
+extends CharacterBody2D
 class_name Player
+
+@export var max_speed:int = 1000
+@export var gravity : float = 55
+@export var jump_force : int = 1600
 
 var morale: int = 20:
 	set(value):
@@ -44,3 +47,33 @@ func print_status():
 
 func string_status() -> String:
 	return "Morale:" + String.num(morale)
+
+
+# ----------- MOVEMENT ------------ #
+	
+func _physics_process(_delta):
+	
+	if not is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 2000:
+			velocity.y = 2000
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x = max_speed 
+		$AnimatedSprite2D.flip_h = false
+		
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = -max_speed
+		$AnimatedSprite2D.flip_h = true
+	else:
+		velocity.x = 0
+
+	if Input.is_action_pressed("jump"):
+		if is_on_floor():
+			velocity.y = -jump_force
+
+	move_and_slide()
+
+
+
+
